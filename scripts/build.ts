5a1cf74d835750326ad8dea7c8f8c02078448a74
winstxnhdw <winstxnhdw@gmail.com>
2023-07-23 02:46:07 +0800

    fix: setup extension configs correctly

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,34 +1,17 @@
 import { build } from 'esbuild'
-import { cp, mkdir, rm } from 'fs/promises'
+import { mkdir, rm } from 'fs/promises'
 import { argv } from 'process'
 
 const build_directory = 'dist'
-const external_modules = [] as string[]
+const external_modules = ['vscode'] as string[]
 
 async function main(args: string[]) {
   await rm(build_directory, { recursive: true, force: true })
   await mkdir(build_directory)
 
-  const results = await Promise.allSettled(
-    external_modules.map((module) =>
-      cp(
-        `node_modules/${module}`,
-        `${build_directory}/node_modules/${module}`,
-        {
-          recursive: true,
-        }
-      )
-    )
-  )
-
-  results.forEach((result) => {
-    if (result.status !== 'rejected') return
-    console.error(result.reason)
-  })
-
   await build({
-    entryPoints: ['src/index.ts'],
-    outfile: `${build_directory}/index.js`,
+    entryPoints: ['src/extension.ts'],
+    outfile: `${build_directory}/extension.js`,
     bundle: true,
     minify: args.slice(2)[0] !== 'test',
     platform: 'node',
